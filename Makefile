2b038103612b17a887b33de7e4109d0586d23e73
Jesse Joseph <jjoseph@cs.hmc.edu>
2017-03-02 14:23:26 -0800

    made new data file for part 2b_1

--- Makefile
+++ Makefile
@@ -2,17 +2,11 @@ GXX=gcc
 FLAGS=-Wall -Wextra -Wpedantic
 PTHREAD=-lpthread
 LISTSOURCES=SortedList.h SortedList.c SortedList_m.c SortedList_m.h SortedList_s.c SortedList_s.h
-MODULES=lab2_add.c lab2_list.c
-DATA=lab2_add.csv lab2_list.csv
-IMAGES=lab2_add-1.png lab2_add-2.png lab2_add-3.png lab2_add-4.png lab2_add-5.png lab2_list-1.png lab2_list-2.png lab2_list-3.png lab2_list-4.png
-TESTS=add_test.sh list_test.sh
+MODULES=lab2_list.c
+DATA=lab2_list.csv
+IMAGES=lab2_list-1.png lab2_list-2.png lab2_list-3.png lab2_list-4.png
+TESTS=list_test.sh
 
-all: lab2_list.c lab2_add.c SortedList.c SortedList_m.c SortedList_s.c
-	make lab2_add
-	make lab2_list
-
-lab2_add: lab2_add.c
-	$(GXX) lab2_add.c -o lab2_add $(PTHREAD) $(FLAGS)
 
 lab2_list: lab2_list.c SortedList.c SortedList_m.c SortedList_s.c
 	make SortedList
@@ -31,18 +25,15 @@ SortedList_m: SortedList_m.h SortedList_m.c
 
 graphs:
 	gnuplot lab2_list.gp
-	gnuplot lab2_add.gp
 
 tests:
-	make all
-	bash add_test.sh
+	make lab2_list
 	bash list_test.sh
 
 dist:
 	tar -czvf lab2a-040161840.tar.gz Makefile README $(LISTSOURCES) $(MODULES) $(DATA) $(IMAGES) $(TESTS)
 
 clean:
-	-rm lab2_add
 	-rm lab2_list
 	-rm SortedList.o
 	-rm SortedList_m.o
--- list_test.sh
+++ list_test.sh
@@ -1,156 +1,19 @@
 #!/bin/bash
-DATAFILE="lab2_list.csv"
+DATAFILE1=lab2b_1.csv
 
-if [ ! -f $DATAFILE ]
+if [ ! -f $DATAFILE1 ]
 then
-	touch $DATAFILE
+	touch $DATAFILE1
 fi
-
-# single thread tests
-for numIters in 10 100 1000 10000 20000
-do
-	echo "THREADS=1 ITERATIONS=$numIters"
-	./lab2_list --iterations=$numIters >> $DATAFILE
-done
-
-# list-none-none tests
-for numIters in 1 10 100 1000
-do
-	for numThreads in 2 4 8 12
-	do
-		echo "THREADS=$numThreads ITERATIONS=$numIters"
-		./lab2_list --iterations=$numIters --threads=$numThreads >> $DATAFILE
-	done
-done
-
-# list-i-none tests
-for numIters in 1 2 4 8 16 32
-do
-	for numThreads in 2 4 8 12
-	do
-		echo "THREADS=$numThreads ITERATIONS=$numIters YIELD=i"
-		./lab2_list --yield=i --iterations=$numIters --threads=$numThreads >> $DATAFILE
-	done
-done
-
-# list-d-none tests
-for numIters in 1 2 4 8 16 32
-do
-	for numThreads in 2 4 8 12
-	do
-		echo "THREADS=$numThreads ITERATIONS=$numIters YIELD=d"
-		./lab2_list --yield=d --iterations=$numIters --threads=$numThreads >> $DATAFILE
-	done
-done
-
-# list-il-none tests
-for numIters in 1 2 4 8 16 32
-do
-	for numThreads in 2 4 8 12
-	do
-		echo "THREADS=$numThreads ITERATIONS=$numIters YIELD=il"
-		./lab2_list --yield=il --iterations=$numIters --threads=$numThreads >> $DATAFILE
-	done
-done
-
-# list-dl-none tests
-for numIters in 1 2 4 8 16 32
-do
-	for numThreads in 2 4 8 12
-	do
-		echo "THREADS=$numThreads ITERATIONS=$numIters YIELD=dl"
-		./lab2_list --yield=dl --iterations=$numIters --threads=$numThreads >> $DATAFILE
-	done
-done
-
-# list-i-s tests
-for numIters in 1 2 4 8 16 32
-do
-	for numThreads in 2 4 8 12
-	do
-		echo "THREADS=$numThreads ITERATIONS=$numIters YIELD=i SYNC=s"
-		./lab2_list --yield=i --iterations=$numIters --threads=$numThreads --sync=s >> $DATAFILE
-	done
-done
-
-# list-d-s tests
-for numIters in 1 2 4 8 16 32
-do
-	for numThreads in 2 4 8 12
-	do
-		echo "THREADS=$numThreads ITERATIONS=$numIters YIELD=d SYNC=s"
-		./lab2_list --yield=d --iterations=$numIters --threads=$numThreads --sync=s >> $DATAFILE
-	done
-done
-
-# list-il-s tests
-for numIters in 1 2 4 8 16 32
-do
-	for numThreads in 2 4 8 12
-	do
-		echo "THREADS=$numThreads ITERATIONS=$numIters YIELD=il SYNC=s"
-		./lab2_list --yield=il --iterations=$numIters --threads=$numThreads --sync=s >> $DATAFILE
-	done
-done
-
-# list-dl-s tests
-for numIters in 1 2 4 8 16 32
-do
-	for numThreads in 2 4 8 12
-	do
-		echo "THREADS=$numThreads ITERATIONS=$numIters YIELD=dl SYNC=s"
-		./lab2_list --yield=dl --iterations=$numIters --threads=$numThreads --sync=s >> $DATAFILE
-	done
-done
-
-# list-i-m tests
-for numIters in 1 2 4 8 16 32
-do
-	for numThreads in 2 4 8 12
-	do
-		echo "THREADS=$numThreads ITERATIONS=$numIters YIELD=i SYNC=m"
-		./lab2_list --yield=i --iterations=$numIters --threads=$numThreads --sync=m >> $DATAFILE
-	done
-done
-
-# list-d-m tests
-for numIters in 1 2 4 8 16 32
-do
-	for numThreads in 2 4 8 12
-	do
-		echo "THREADS=$numThreads ITERATIONS=$numIters YIELD=d SYNC=m"
-		./lab2_list --yield=d --iterations=$numIters --threads=$numThreads --sync=m >> $DATAFILE
-	done
-done
-
-# list-il-m tests
-for numIters in 1 2 4 8 16 32
-do
-	for numThreads in 2 4 8 12
-	do
-		echo "THREADS=$numThreads ITERATIONS=$numIters YIELD=il SYNC=m"
-		./lab2_list --yield=il --iterations=$numIters --threads=$numThreads --sync=m >> $DATAFILE
-	done
-done
-
-# list-dl-m tests
-for numIters in 1 2 4 8 16 32
-do
-	for numThreads in 2 4 8 12
-	do
-		echo "THREADS=$numThreads ITERATIONS=$numIters YIELD=dl SYNC=m"
-		./lab2_list --yield=dl --iterations=$numIters --threads=$numThreads --sync=m >> $DATAFILE
-	done
-done
 
 for numThreads in 1 2 4 8 12 16 24
 do
 	echo "THREADS=$numThreads ITERATIONS=1000 SYNC=s"
-	./lab2_list --iterations=1000 --threads=$numThreads --sync=s >> $DATAFILE
+	./lab2_list --iterations=1000 --threads=$numThreads --sync=s >> $DATAFILE1
 done
 
 for numThreads in 1 2 4 8 12 16 24
 do
 	echo "THREADS=$numThreads ITERATIONS=1000 SYNC=m"
-	./lab2_list --iterations=1000 --threads=$numThreads --sync=m >> $DATAFILE
+	./lab2_list --iterations=1000 --threads=$numThreads --sync=m >> $DATAFILE1
 done
